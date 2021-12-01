3b91866256e4597ad38532d1d33e622106890cc7
NirLabin <labinski10@gmail.com>
2021-12-01 12:12:41 +0200

    add-task-cmp

---
+++ src/components/add-task.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="task-add">
+    <input
+      type="text"
+      v-model="title"
+      @focus="showAddBtn"
+      placeholder="Add task"
+    />
+    <button v-if="addBtn" class="btn" @click="addNewTask">+Add task</button>
+  </section>
+</template>
+
+<script>
+import utilService from '@/services/util.service.js';
+
+export default {
+  name: 'taskAdd',
+  props: {},
+  data() {
+    return {
+      title: '',
+      addBtn: false,
+    };
+  },
+  created() {},
+  methods: {
+    addNewTask() {
+      console.log(this.title);
+      const taskId = utilService.makeId;
+      const title = this.title;
+      this.$emit('addTask', { title, taskId });
+      this.addBtn = !this.addBtn;
+      this.title = '';
+    },
+    showAddBtn() {
+      this.addBtn = true;
+    },
+  },
+  components: {},
+};
+</script>
--- src/components/group-list.vue
+++ src/components/group-list.vue
@@ -1,48 +1,52 @@
 <template>
-	<section class="group-list-container">
-		<ul v-if="groups && groups.length" class="group-list clean-list">
-			<li class="group" v-for="(group, idx) in groups" :key="idx">
-				<group-header :group="group" />
-				<taskList :group="group" />
-			</li>
-		</ul>
-		<!-- <button class="btn" :style="{ color: groupToEdit?.color || '#fff' }" @click="addGroup"> -->
-		<button class="btn" :style="styleObj" @click="addGroup">+ Add new group</button>
-	</section>
+  <section class="group-list-container">
+    <ul v-if="groups && groups.length" class="group-list clean-list">
+      <li class="group" v-for="(group, idx) in groups" :key="idx">
+        <group-header :group="group" />
+        <taskList :group="group" />
+      </li>
+    </ul>
+    <!-- <button class="btn" :style="{ color: groupToEdit?.color || '#fff' }" @click="addGroup"> -->
+    <add-task @addTask="saveTask" />
+  </section>
 </template>
 
 <script>
 import taskList from '@/components/task-list';
 import groupHeader from '@/components/group-header';
+import addTask from './add-task.vue';
 export default {
-	name: 'groupList',
-	props: {
-		boards: {
-			type: Array,
-			required: true,
-		},
-	},
-	data() {
-		return {
-			groupToEdit: {},
-			groups: [],
-			styleObj: {},
-			showRename: false,
-		};
-	},
-	created() {
-		this.groupToEdit = this.$store.getters.groupToEdit;
-		this.styleObj = { color: this.groupToEdit?.style.clr || '#000' };
-		this.groups = this.boards[0].groups;
-	},
-	methods: {
-		renameGroup() {
-			this.showRename = !this.showRename;
-		},
-		addGroup() {
-			this.$emit('addGroup', this.groupToEdit);
-		},
-	},
-	components: { taskList, groupHeader },
+  name: 'groupList',
+  props: {
+    boards: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      groupToEdit: {},
+      groups: [],
+      styleObj: {},
+      showRename: false,
+    };
+  },
+  created() {
+    this.groupToEdit = this.$store.getters.groupToEdit;
+    this.styleObj = { color: this.groupToEdit?.style.clr || '#000' };
+    this.groups = this.boards[0].groups;
+  },
+  methods: {
+    renameGroup() {
+      this.showRename = !this.showRename;
+    },
+    addGroup() {
+      this.$emit('addGroup', this.groupToEdit);
+    },
+    saveTask(task) {
+      this.$store.dispatch({ type: 'addTask', task });
+    },
+  },
+  components: { taskList, groupHeader, addTask },
 };
 </script>
