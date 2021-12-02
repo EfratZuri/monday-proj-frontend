735e84cfa24bb768c1c181cca06feaae97ce2938
EfratZuri <efratzuri1@gmail.com>
2021-12-02 15:25:11 +0200

    Changes in class in preview

--- src/views/workspace.vue
+++ src/views/workspace.vue
@@ -1,53 +1,53 @@
 <template>
-  <div class="workspace">
-    <div v-if="isLoading"><p>Loading</p></div>
-    <!-- <router-link> to: </router-link> -->
-    <div v-else>
-      <boardHeader
-        :activeBoard="activeBoard"
-        @addItem="addItem"
-        @saveBoard="saveBoard"
-        @addGroup="addGroup"
-      />
-      <groupList :board="activeBoard" @addGroup="addGroup" />
-    </div>
-  </div>
+	<div class="workspace">
+		<div v-if="isLoading"><p>Loading</p></div>
+		<!-- <router-link> to: </router-link> -->
+		<div v-else>
+			<boardHeader
+				:activeBoard="activeBoard"
+				@addItem="addItem"
+				@saveBoard="saveBoard"
+				@addGroup="addGroup"
+			/>
+			<groupList :board="activeBoard" @addGroup="addGroup" />
+		</div>
+	</div>
 </template>
 
 <script>
 import boardHeader from '@/components/board-header.vue';
 import groupList from '@/components/group-list.vue';
 export default {
-  name: 'workspace',
-  async created() {
-    this.$store.dispatch({ type: 'loadBoards' });
-  },
-  computed: {
-    isLoading() {
-      return this.$store.getters.isLoading;
-    },
-    boards() {
-      return this.$store.getters.boards;
-    },
-    activeBoard() {
-      return this.$store.getters.activeBoard;
-    },
-  },
-  methods: {
-    addItem() {
-      console.log('Add item');
-    },
-    addGroup(group) {
-      // group.boardId = this.$store.getters.activeBoard._id;
-      this.$store.dispatch({ type: 'saveGroup', group });
-    },
-    saveBoard(board) {
-      this.$store.dispatch({ type: 'saveBoard', board });
-    },
-  },
-  components: {
-    groupList,
-    boardHeader,
-  },
+	name: 'workspace',
+	async created() {
+		this.$store.dispatch({ type: 'loadBoards' });
+	},
+	computed: {
+		isLoading() {
+			return this.$store.getters.isLoading;
+		},
+		boards() {
+			return this.$store.getters.boards;
+		},
+		activeBoard() {
+			return this.$store.getters.activeBoard;
+		},
+	},
+	methods: {
+		addItem() {
+			console.log('Add item');
+		},
+		addGroup(group) {
+			// group.boardId = this.$store.getters.activeBoard._id;
+			this.$store.dispatch({ type: 'saveGroup', group });
+		},
+		saveBoard(board) {
+			this.$store.dispatch({ type: 'saveBoard', board });
+		},
+	},
+	components: {
+		groupList,
+		boardHeader,
+	},
 };
 </script>
