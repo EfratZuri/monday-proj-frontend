0eef8e84f968808d01268c4ff7d02d8dba744305
EfratZuri <efratzuri1@gmail.com>
2021-12-02 10:44:35 +0200

    Changes in board service

--- src/components/component.vue
+++ src/components/component.vue
@@ -1,49 +1,50 @@
 <template>
-  <section class="component">
-    <div :class="[is]" :style="styleObj">
-      <span>{{ selectedToDisplay }}</span>
-    </div>
+	<section class="component">
+		<div :class="[is]" :style="styleObj">
+			<span>{{ selectedToDisplay }}</span>
+		</div>
 
-    <!-- <ul v-if="options && options.length" class="clean-list">
+		<!-- <ul v-if="options && options.length" class="clean-list">
 			<li v-for="(option, idx) in options" :key="idx">
 				<span @click="update(option, $event)">{{ option }}</span>
 			</li>
 		</ul> -->
-  </section>
+	</section>
 </template>
 
 <script>
 export default {
-  name: 'component',
-  props: ['info', 'is'],
-  data() {
-    return { options: null, selected: null, styleObj: {} };
-  },
-  created() {
-    if (!this.info) {
-      const hey = '';
-      console.log(hey);
-    } else {
-      const keys = Object.keys(this.info);
-      const { optionKey, selectedKey } = keys.reduce((acc, key) => {
-        acc[`${key.includes('selected') ? 'selected' : 'option'}Key`] = key;
-        return acc;
-      }, {});
-      // this.options = this.info[optionKey];
-      console.log(optionKey);
-      this.selected = this.info[selectedKey];
-    }
-  },
-  methods: {
-    update(curType, event) {
-      this.$emit(curType, event);
-    },
-  },
-  computed: {
-    selectedToDisplay() {
-      // return `${}`
-      return this.selected || '';
-    },
-  },
+	name: 'component',
+	props: ['info', 'is'],
+	data() {
+		return { options: null, selected: null, styleObj: {} };
+	},
+	created() {
+		if (!this.info) {
+			const hey = '';
+			console.log(hey);
+		} else {
+			const keys = Object.keys(this.info);
+			const { optionKey, selectedKey } = keys.reduce((acc, key) => {
+				acc[`${key.includes('selected') ? 'selected' : 'option'}Key`] = key;
+				return acc;
+			}, {});
+
+			// this.options = this.info[optionKey];
+			console.log(optionKey);
+			this.selected = this.info[selectedKey];
+		}
+	},
+	methods: {
+		update(curType, event) {
+			this.$emit(curType, event);
+		},
+	},
+	computed: {
+		selectedToDisplay() {
+			// return `${}`
+			return this.selected || '';
+		},
+	},
 };
 </script>
