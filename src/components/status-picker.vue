c7b343f5cd504ed14cb1a3e71d72efa98d49bdac
NirLabin <labinski10@gmail.com>
2021-12-02 17:12:13 +0200

    task-crud

--- src/components/status-picker.vue
+++ src/components/status-picker.vue
@@ -1,44 +1,43 @@
 <template>
-	<div v-if="styleObj" class="col-cell" :style="styleObj">
-		<span>{{ infoForDisplay }} </span>
-	</div>
+  <div v-if="styleObj" class="col-cell" :style="styleObj">
+    <span>{{ infoForDisplay }} </span>
+  </div>
 </template>
 
 <script>
 export default {
-	name: 'statusPicker',
-	props: ['info'],
+  name: 'statusPicker',
+  props: ['info'],
 
-	data() {
-		return {
-			styleObj: null,
-			selectedObj: {},
-		};
-	},
-	created() {
-		console.log('Status picker created');
-		const selectedName = this.info.selected;
-		console.log('selcted name', selectedName);
-		if (selectedName?.name === 'default') {
-			this.styleObj = selectedName.style;
-		} else {
-			this.selectedObj = this.info.opts.find(({ name }) => name === selectedName);
-			this.styleObj = this.selectedObj.style;
-		}
-		console.log(this.styleObj);
-	},
-	methods: {
-		update(curType, event) {
-			this.$emit(curType, event);
-		},
-		toggleOptions() {
-			this.showOptions = !this.showOptions;
-		},
-	},
-	computed: {
-		infoForDisplay() {
-			return this.selectedObj.name === 'default' ? '' : this.selectedObj.name;
-		},
-	},
+  data() {
+    return {
+      styleObj: null,
+      selectedObj: {},
+    };
+  },
+  created() {
+    const selectedName = this.info.selected;
+    if (selectedName?.name === 'default') {
+      this.styleObj = selectedName.style;
+    } else {
+      this.selectedObj = this.info.opts.find(
+        ({ name }) => name === selectedName
+      );
+      this.styleObj = this.selectedObj.style;
+    }
+  },
+  methods: {
+    update(curType, event) {
+      this.$emit(curType, event);
+    },
+    toggleOptions() {
+      this.showOptions = !this.showOptions;
+    },
+  },
+  computed: {
+    infoForDisplay() {
+      return this.selectedObj.name === 'default' ? '' : this.selectedObj.name;
+    },
+  },
 };
 </script>
--- src/services/board.service.js
+++ src/services/board.service.js
@@ -5,87 +5,99 @@ import { socketService, SOCKET_EVENT_REVIEW_ADDED } from './socket.service';
 const STORAGE_KEY_BOARDS = 'boards';
 
 export const boardService = {
-	query,
-	remove,
-	getEmptyBoard,
-	getEmptyGroup,
-	getEmptyTask,
-	getEmptyActivity,
-	saveGroup,
-	saveTask,
-	saveBoard,
+  query,
+  remove,
+  getEmptyBoard,
+  getEmptyGroup,
+  getEmptyTask,
+  getEmptyActivity,
+  saveGroup,
+  saveTask,
+  deleteTask,
+  saveBoard,
 };
 
 async function query(filterBy = {}) {
-	console.log('', filterBy);
-	// let queryStr = !filterBy ? '' : `?name=${filterBy.name}&sort=anaAref`;
-	// return httpService.get(`board${queryStr}`)
-	let boards = await storageService.query(STORAGE_KEY_BOARDS);
-	if (!boards.length) {
-		const board = _createBoard();
-		boards.push(board);
-		saveBoard(board);
-	}
-	return boards;
+  console.log('', filterBy);
+  // let queryStr = !filterBy ? '' : `?name=${filterBy.name}&sort=anaAref`;
+  // return httpService.get(`board${queryStr}`)
+  let boards = await storageService.query(STORAGE_KEY_BOARDS);
+  if (!boards.length) {
+    const board = _createBoard();
+    boards.push(board);
+    saveBoard(board);
+  }
+  return boards;
 }
 
 // BOARD
 
 async function remove(boardId) {
-	// return httpService.delete(`board/${boardId}`)
-	return await storageService.delete('board', boardId);
+  // return httpService.delete(`board/${boardId}`)
+  return await storageService.delete('board', boardId);
 }
 
 async function saveBoard(board) {
-	// const addedBoard = await httpService.post(`board`, board)
-	let savedBoard;
-	try {
-		if (board._id) savedBoard = await storageService.put(STORAGE_KEY_BOARDS, board);
-		else savedBoard = await storageService.post(STORAGE_KEY_BOARDS, board);
-	} catch (error) {
-		console.log('error', error);
-	}
-	return savedBoard;
+  // const addedBoard = await httpService.post(`board`, board)
+  let savedBoard;
+  try {
+    if (board._id)
+      savedBoard = await storageService.put(STORAGE_KEY_BOARDS, board);
+    else savedBoard = await storageService.post(STORAGE_KEY_BOARDS, board);
+  } catch (error) {
+    console.log('error', error);
+  }
+  return savedBoard;
 }
 
 async function getById(boardId) {
-	const boards = await query();
-	const board = boards.find(({ _id }) => _id === boardId);
-	return board;
+  const boards = await query();
+  const board = boards.find(({ _id }) => _id === boardId);
+  return board;
 }
 
 // Save task
 
 async function saveTask(boardId, task, groupId, activity) {
-	const board = await getById(boardId);
-	const group = board.groups.find(({ _id }) => _id == groupId);
-	if (!task._id) {
-		task._id = utilService.makeId();
-		group.tasks.push(task);
-	} else {
-		const idx = group.findIndex(({ _id }) => _id === task._id);
-		group.tasks.splice(idx, 1, task);
-	}
-	board.activities.unshift(activity);
-	saveBoard(board);
-	return board;
+  const board = await getById(boardId);
+  const group = board.groups.find(({ _id }) => _id === groupId);
+  if (!task._id) {
+    task._id = utilService.makeId();
+    group.tasks.push(task);
+  } else {
+    const idx = group.tasks.findIndex(({ _id }) => _id === task._id);
+    group.tasks.splice(idx, 1, task);
+  }
+  board.activities.unshift(activity);
+  saveBoard(board);
+  return board;
+}
+
+async function deleteTask(boardId, task, groupId, activity) {
+  const board = await getById(boardId);
+  const group = board.groups.find(({ _id }) => _id === groupId);
+  const idx = group.tasks.findIndex(({ _id }) => _id === task._id);
+  group.tasks.splice(idx, 1);
+  board.activities.unshift(activity);
+  saveBoard(board);
+  return board;
 }
 
 // Save group
 
 async function saveGroup(group, activeBoardId) {
-	const board = await getById(activeBoardId);
-	// If the group is new
-	if (!group._id) {
-		group._id = utilService.makeId();
-		board.groups.push(group);
-		// Update group
-	} else {
-		const idx = board.groups.findIndex(({ _id }) => _id === group._id);
-		board.groups.splice(idx, 1, group);
-	}
-	saveBoard(board);
-	return group;
+  const board = await getById(activeBoardId);
+  // If the group is new
+  if (!group._id) {
+    group._id = utilService.makeId();
+    board.groups.push(group);
+    // Update group
+  } else {
+    const idx = board.groups.findIndex(({ _id }) => _id === group._id);
+    board.groups.splice(idx, 1, group);
+  }
+  saveBoard(board);
+  return group;
 }
 
 // More ways to send query params:
@@ -94,53 +106,62 @@ async function saveGroup(group, activeBoardId) {
 // This IIFE functions for Dev purposes
 // It allows testing of real time updates (such as sockets) by listening to storage events
 (async () => {
-	let boards = await storageService.query('board');
+  let boards = await storageService.query('board');
 
-	// Dev Helper: Listens to when localStorage changes in OTHER browser
-	window.addEventListener('storage', async () => {
-		console.log('Storage updated');
-		const freshBoards = await storageService.query('board');
-		if (freshBoards.length === boards.length + 1) {
-			console.log('Board Added - localStorage updated from another browser');
-			socketService.emit(SOCKET_EVENT_REVIEW_ADDED, freshBoards[freshBoards.length - 1]);
-		}
-		boards = freshBoards;
-	});
+  // Dev Helper: Listens to when localStorage changes in OTHER browser
+  window.addEventListener('storage', async () => {
+    console.log('Storage updated');
+    const freshBoards = await storageService.query('board');
+    if (freshBoards.length === boards.length + 1) {
+      console.log('Board Added - localStorage updated from another browser');
+      socketService.emit(
+        SOCKET_EVENT_REVIEW_ADDED,
+        freshBoards[freshBoards.length - 1]
+      );
+    }
+    boards = freshBoards;
+  });
 })();
 
 // GET EMPTY
 
 function getEmptyGroup(clr) {
-	return { title: 'New Group', _id: '', tasks: [], style: { clr } };
+  return { title: 'New Group', _id: '', tasks: [], style: { clr } };
 }
 
 function getEmptyTask() {
-	return { title: '', _id: '', cmps: {} };
+  return { title: '', _id: '', cmps: {} };
 }
 
 function getEmptyActivity() {
-	return { txt: '', _id: '', cratedAt: '', byMember: {}, task: {} };
+  return { txt: '', _id: '', cratedAt: '', byMember: {}, task: {} };
 }
 
 function getEmptyBoard() {
-	return {
-		title: 'Board',
-		createdAt: '',
-		_id: '',
-		createdBy: {},
-		members: [],
-		groups: [],
-		activities: [],
-		cmpsOrder: ['status-picker', 'member-picker', 'date-picker'],
-		cols: [
-			{ type: 'statusPicker', data: { opts: _getStatusOptions() } },
-			{ type: 'memberPicker', data: { opts: [{ name: 'default', display: '', style: {} }] } },
-			{ type: 'datePicker', data: { opts: [{ name: 'default', display: '', style: {} }] } },
-		],
+  return {
+    title: 'Board',
+    createdAt: '',
+    _id: '',
+    createdBy: {},
+    members: [],
+    groups: [],
+    activities: [],
+    cmpsOrder: ['status-picker', 'member-picker', 'date-picker'],
+    cols: [
+      { type: 'statusPicker', data: { opts: _getStatusOptions() } },
+      {
+        type: 'memberPicker',
+        data: { opts: [{ name: 'default', display: '', style: {} }] },
+      },
+      {
+        type: 'datePicker',
+        data: { opts: [{ name: 'default', display: '', style: {} }] },
+      },
+    ],
 
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ultrices lectus vitae lectus accumsan, ac convallis sem ultricies. Aliquam sagittis cursus sollicitudin. Etiam feugiat diam turpis, sit amet finibus ligula malesuada sed. ',
-	};
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ultrices lectus vitae lectus accumsan, ac convallis sem ultricies. Aliquam sagittis cursus sollicitudin. Etiam feugiat diam turpis, sit amet finibus ligula malesuada sed. ',
+  };
 }
 
 // function getEmptyCmp() {
@@ -150,38 +171,38 @@ function getEmptyBoard() {
 // Auxiliary functions
 
 function _createBoard() {
-	const board = getEmptyBoard();
+  const board = getEmptyBoard();
 
-	board.groups.push(getEmptyGroup('red'));
-	board.groups.push(getEmptyGroup('blue'));
-	board.groups.push(getEmptyGroup('green'));
-	board.groups.forEach((group) => (group._id = utilService.makeId()));
-	return board;
+  board.groups.push(getEmptyGroup('red'));
+  board.groups.push(getEmptyGroup('blue'));
+  board.groups.push(getEmptyGroup('green'));
+  board.groups.forEach((group) => (group._id = utilService.makeId()));
+  return board;
 }
 function _getStatusOptions() {
-	return [
-		{
-			name: 'default',
-			display: '',
-			style: {
-				backgroundColor: 'rgb(196, 196, 196)',
-			},
-		},
-		{
-			name: 'stuck',
-			display: 'Stuck',
-			style: {
-				backgroundColor: 'rgb(226, 68, 92)',
-			},
-		},
-		{
-			name: 'done',
-			display: 'Done',
-			style: {
-				backgroundColor: 'rgb(0, 200, 117)',
-			},
-		},
-	];
+  return [
+    {
+      name: 'default',
+      display: '',
+      style: {
+        backgroundColor: 'rgb(196, 196, 196)',
+      },
+    },
+    {
+      name: 'stuck',
+      display: 'Stuck',
+      style: {
+        backgroundColor: 'rgb(226, 68, 92)',
+      },
+    },
+    {
+      name: 'done',
+      display: 'Done',
+      style: {
+        backgroundColor: 'rgb(0, 200, 117)',
+      },
+    },
+  ];
 }
 /*
 'status-picker': {
