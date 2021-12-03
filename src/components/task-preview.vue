df166744ee5a1a48e337af273b6d1ff514d5f6ef
EfratZuri <efratzuri1@gmail.com>
2021-12-03 13:14:57 +0200

    Changes in style

--- src/components/task-preview.vue
+++ src/components/task-preview.vue
@@ -1,53 +1,41 @@
 <template>
-  <section
-    class="task-preview flex-def"
-    @mouseover="showEditBtn = true"
-    @mouseleave="showEditBtn = false"
-  >
-    <div class="name-cell flex align-center" :class="{ selected: isSelected }">
-      <div
-        class="pulse-left-indicator"
-        :style="{ backgroundColor: styleObj.clr, color: styleObj.clr }"
-      >
-        <div class="left-indicator-inner"></div>
-      </div>
-      <div class="task-title">
-        <!-- <button class="btn" @click="deleteTask">X</button> -->
-        <span v-if="!showEditTask">
-          {{ task.title }}
-        </span>
-        <input
-          v-else
-          type="text"
-          ref="taskTitle"
-          v-model="taskToEdit.title"
-          @blur="saveTitle"
-          @keyup.enter="$event.target.blur()"
-        />
-        <div class="edit-btn-wrapper">
-          <button v-if="showEditBtn" class="btn btn-edit" @click="toggleEdit">
-            EDIT
-          </button>
-        </div>
-      </div>
-    </div>
-
-    <div class="grid-cells-row-component">
-      <component
-        v-for="(col, idx) in cols"
-        :key="idx"
-        :is="col.type"
-        :info="getCmpInfo(col)"
-      />
-    </div>
-    <!-- <div class="grid-cells-row-component-wrapper">
-			<ul v-if="cols && cols.length" class="col-list clean-list flex-def">
-				<li v-for="(col, idx) in cols" :key="idx" class="col-cell">
-					<component :is="col.type" :info="getCmpInfo(col)" />
-				</li>
-			</ul>
-		</div> -->
-  </section>
+	<section
+		class="grid-row-component task-preview flex-def"
+		@mouseover="showEditBtn = true"
+		@mouseleave="showEditBtn = false"
+	>
+		<div
+			class="grid-cell-row-component-header name-cell flex align-center"
+			:class="{ selected: isSelected }"
+		>
+			<div
+				class="pulse-left-indicator"
+				:style="{ backgroundColor: styleObj.clr, color: styleObj.clr }"
+			>
+				<div class="left-indicator-inner"></div>
+			</div>
+			<div class="task-title">
+				<!-- <button class="btn" @click="deleteTask">X</button> -->
+				<span v-if="!showEditTask">
+					{{ task.title }}
+				</span>
+				<input
+					v-else
+					type="text"
+					ref="taskTitle"
+					v-model="taskToEdit.title"
+					@blur="saveTitle"
+					@keyup.enter="$event.target.blur()"
+				/>
+				<div class="edit-btn-wrapper">
+					<button v-if="showEditBtn" class="btn btn-edit" @click="toggleEdit">EDIT</button>
+				</div>
+			</div>
+		</div>
+		<div class="grid-cells-row-component">
+			<component v-for="(col, idx) in cols" :key="idx" :is="col.type" :info="getCmpInfo(col)" />
+		</div>
+	</section>
 </template>
 
 <script>
@@ -55,74 +43,74 @@ import statusPicker from '../components/status-picker';
 import datePicker from '../components/date-picker';
 import memberPicker from '../components/member-picker';
 export default {
-  name: 'taskPreview',
-  props: {
-    task: {
-      type: Object,
-      required: true,
-    },
-    styleObj: {
-      type: Object,
-    },
-  },
-  data() {
-    return {
-      cols: null,
-      cmpsOrder: null,
-      info: null,
-      taskToEdit: null,
-      showEditTask: false,
-      showEditBtn: false,
-      isSelected: false,
-    };
-  },
-  created() {
-    this.taskToEdit = { ...this.task };
-    this.cmpsOrder = this.$store.getters.cmpsOrder;
-    this.cols = this.$store.getters.cols;
-  },
-  methods: {
-    updateTask(curType, e) {
-      console.log(e, curType);
-    },
+	name: 'taskPreview',
+	props: {
+		task: {
+			type: Object,
+			required: true,
+		},
+		styleObj: {
+			type: Object,
+		},
+	},
+	data() {
+		return {
+			cols: null,
+			cmpsOrder: null,
+			info: null,
+			taskToEdit: null,
+			showEditTask: false,
+			showEditBtn: false,
+			isSelected: false,
+		};
+	},
+	created() {
+		this.taskToEdit = { ...this.task };
+		this.cmpsOrder = this.$store.getters.cmpsOrder;
+		this.cols = this.$store.getters.cols;
+	},
+	methods: {
+		updateTask(curType, e) {
+			console.log(e, curType);
+		},
 
-    toggleSelected() {
-      this.isSelected = !this.isSelected;
-    },
-    async toggleEdit() {
-      this.toggleSelected();
-      await (this.showEditTask = !this.showEditTask);
-      if (this.$refs.taskTitle) this.$refs.taskTitle.focus();
-    },
+		toggleSelected() {
+			this.isSelected = !this.isSelected;
+		},
+		async toggleEdit() {
+			this.toggleSelected();
+			await (this.showEditTask = !this.showEditTask);
+			if (this.$refs.taskTitle) this.$refs.taskTitle.focus();
+		},
 
-    saveTitle() {
-      this.toggleEdit();
-      this.$emit('saveTitle', this.taskToEdit);
-    },
+		saveTitle() {
+			this.toggleEdit();
+			this.$emit('saveTitle', this.taskToEdit);
+		},
 
-    deleteTask() {
-      this.getTaskToEdit();
-      this.$emit('deleteTask', this.taskToEdit);
-    },
+		deleteTask() {
+			this.getTaskToEdit();
+			this.$emit('deleteTask', this.taskToEdit);
+		},
 
-    getCmpInfo(col) {
-      if (!this.task?.[col.type]) return this.getDefaultCmp(col);
+		getCmpInfo(col) {
+			if (!this.task?.[col.type]) return this.getDefaultCmp(col);
 
-      return { selected: this.task[col.type], opts: col.data.opts };
-    },
-    getDefaultCmp(col) {
-      // return { selected: .options[cmp].default, options: this.cols[cmp].options };
-      return {
-        selected: col.data.opts.find(({ name }) => name === 'default'),
-        opts: col.data.opts,
-      };
-    },
-  },
-  components: {
-    statusPicker,
-    datePicker,
-    memberPicker,
-  },
-  // <!-- @blur="saveTitle" -->
+			return { selected: this.task[col.type], opts: col.data.opts };
+		},
+		getDefaultCmp(col) {
+			// return { selected: .options[cmp].default, options: this.cols[cmp].options };
+			return {
+				selected: col.data.opts.find(({ name }) => name === 'default'),
+				opts: col.data.opts,
+			};
+		},
+	},
+	components: {
+		statusPicker,
+		datePicker,
+		memberPicker,
+	},
+	// <!-- @blur="saveTitle" -->
 };
 </script>
--- src/views/workspace.vue
+++ src/views/workspace.vue
@@ -1,60 +1,57 @@
 <template>
-  <div class="workspace">
-    <!-- <router-view /> -->
-    <div v-if="isLoading"><p>Loading</p></div>
-    <div v-else>
-      <boardHeader
-        :activeBoard="activeBoard"
-        @addTask="addTask"
-        @saveBoard="saveBoard"
-        @addGroup="addGroup"
-      />
-      <groupList
-        :board="activeBoard"
-        @addGroup="addGroup"
-        @removeGroup="removeGroup"
-      />
-    </div>
-  </div>
+	<div class="workspace">
+		<!-- <router-view /> -->
+
+		<div v-if="isLoading"><p>Loading</p></div>
+		<div v-else>
+			<boardHeader
+				:activeBoard="activeBoard"
+				@addTask="addTask"
+				@saveBoard="saveBoard"
+				@addGroup="addGroup"
+			/>
+			<groupList :board="activeBoard" @addGroup="addGroup" @removeGroup="removeGroup" />
+		</div>
+	</div>
 </template>
 
 <script>
 import boardHeader from '@/components/board-header.vue';
 import groupList from '@/components/group-list.vue';
 export default {
-  name: 'workspace',
-  async created() {
-    this.$store.dispatch({ type: 'loadBoards' });
-  },
-  computed: {
-    isLoading() {
-      return this.$store.getters.isLoading;
-    },
-    boards() {
-      return this.$store.getters.boards;
-    },
-    activeBoard() {
-      return this.$store.getters.activeBoard;
-    },
-  },
-  methods: {
-    addTask() {
-      this.$store.dispatch({ type: 'addTask' });
-    },
-    addGroup(group) {
-      // group.boardId = this.$store.getters.activeBoard._id;
-      this.$store.dispatch({ type: 'saveGroup', group });
-    },
-    removeGroup(group) {
-      this.$store.dispatch({ type: 'removeGroup', group });
-    },
-    saveBoard(board) {
-      this.$store.dispatch({ type: 'saveBoard', board });
-    },
-  },
-  components: {
-    groupList,
-    boardHeader,
-  },
+	name: 'workspace',
+	async created() {
+		this.$store.dispatch({ type: 'loadBoards' });
+	},
+	computed: {
+		isLoading() {
+			return this.$store.getters.isLoading;
+		},
+		boards() {
+			return this.$store.getters.boards;
+		},
+		activeBoard() {
+			return this.$store.getters.activeBoard;
+		},
+	},
+	methods: {
+		addTask() {
+			this.$store.dispatch({ type: 'addTask' });
+		},
+		addGroup(group) {
+			// group.boardId = this.$store.getters.activeBoard._id;
+			this.$store.dispatch({ type: 'saveGroup', group });
+		},
+		removeGroup(group) {
+			this.$store.dispatch({ type: 'removeGroup', group });
+		},
+		saveBoard(board) {
+			this.$store.dispatch({ type: 'saveBoard', board });
+		},
+	},
+	components: {
+		groupList,
+		boardHeader,
+	},
 };
 </script>
