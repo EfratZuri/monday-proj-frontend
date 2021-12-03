7dca1bd5f639b4fdd719194baa3ef6d4b1360a56
Evgey <evgeny12712@gmail.com>
2021-12-03 10:04:14 +0200

    add task setup

--- src/components/board-header-view-bar.vue
+++ src/components/board-header-view-bar.vue
@@ -1,45 +1,56 @@
 <template>
-	<section class="board-header-view-bar flex-def">
-		<div class="add-task-btn-component flex align-center">
-			<button class="btn btn-blue btn-size-small flex align-center">New task</button>
-			<div class="dropdown-button small-btn">
-				<ion-icon name="chevron-down-outline" />
-			</div>
-		</div>
-		<button class="btn btn-blue btn-size-small" @click="addGroup">New group</button>
-		<ion-icon name="search-outline"></ion-icon>
-		<input type="search" :class="{ focusVisible: isFocusVisible }" placeholder="Search" />
-		<button class="btn btn-icon">Preson</button>
-		<button class="btn" @click="filter">Filter</button>
-		<button class="btn btn-icon">Sort</button>
-		<button class="btn btn-icon">Pin colums</button>
-		<button class="btn btn-icon">Sort</button>
-	</section>
+  <section class="board-header-view-bar flex-def">
+    <div class="add-task-btn-component flex align-center">
+      <button
+        class="btn btn-blue btn-size-small flex align-center"
+        @click="addTask"
+      >
+        New Task
+      </button>
+      <div class="dropdown-button small-btn">
+        <ion-icon name="chevron-down-outline" />
+      </div>
+    </div>
+    <button class="btn btn-blue btn-size-small" @click="addGroup">
+      New group
+    </button>
+    <ion-icon name="search-outline"></ion-icon>
+    <input
+      type="search"
+      :class="{ focusVisible: isFocusVisible }"
+      placeholder="Search"
+    />
+    <button class="btn btn-icon">Preson</button>
+    <button class="btn" @click="filter">Filter</button>
+    <button class="btn btn-icon">Sort</button>
+    <button class="btn btn-icon">Pin colums</button>
+    <button class="btn btn-icon">Sort</button>
+  </section>
 </template>
 
 <script>
 export default {
-	name: 'boardHeaderViewBar',
-	data() {
-		return {
-			isFocusVisible: false,
-		};
-	},
-	created() {},
-	methods: {
-		filter(e) {
-			console.log(e.target.classList.add());
-		},
-		// addTask() {
-		//   this.$emit("addTask");
-		// },
-		addGroup() {
-			this.$emit('addGroup');
-		},
-		toggleFocusVisible() {
-			this.isFocusVisible = !this.isFocusVisible;
-		},
-	},
-	computed: {},
+  name: 'boardHeaderViewBar',
+  data() {
+    return {
+      isFocusVisible: false,
+    };
+  },
+  created() {},
+  methods: {
+    filter(e) {
+      console.log(e.target.classList.add());
+    },
+    addTask() {
+      this.$emit('addTask');
+    },
+    addGroup() {
+      this.$emit('addGroup');
+    },
+    toggleFocusVisible() {
+      this.isFocusVisible = !this.isFocusVisible;
+    },
+  },
+  computed: {},
 };
 </script>
