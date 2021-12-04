2518a9c6ba31e7b97d54516e6c58d7561f8cb301
Evgey <evgeny12712@gmail.com>
2021-12-04 12:45:09 +0200

    'header-work'

---
+++ src/components/board-header-filter-filter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="filter-filter-wrapper flex">
+    <div class="filter-icon-wrapper">
+      <ion-icon name="funnel-outline"></ion-icon>
+    </div>
+    <span class="btn filter-span" @click="filter"> Filter</span>
+    <div class="filter-dropdown-btn dropdown-button small-btn">
+      <ion-icon name="chevron-down-outline" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/components/board-header-filter-person.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="person-filter-wrapper flex">
+    <div class="person-icon-wrapper">
+      <ion-icon name="person-circle-outline"></ion-icon>
+    </div>
+    <span class="person-span">Person</span>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
