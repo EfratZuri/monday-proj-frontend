93f5202a386c1efffef8c7c34a4bc1e54c1fd9ed
NirLabin <labinski10@gmail.com>
2021-12-04 14:05:34 +0200

    update-panel

--- src/components/task-preview.vue
+++ src/components/task-preview.vue
@@ -1,57 +1,71 @@
 <template>
-	<section
-		class="grid-row-component task-preview flex-def"
-		@mouseover="showEditBtn = true"
-		@mouseleave="showEditBtn = false"
-	>
-		<div
-			class="grid-cell-row-component-header name-cell flex align-center"
-			:class="{ selected: isCellSelected }"
-			@click="togglePostPanel"
-		>
-			<!-- <div class="menu-edit-task-container" :class="{ selected: showTaskMenu }">
+  <section
+    class="grid-row-component task-preview flex-def"
+    @mouseover="showEditBtn = true"
+    @mouseleave="showEditBtn = false"
+  >
+    <div
+      class="grid-cell-row-component-header name-cell flex align-center"
+      :class="{ selected: isCellSelected }"
+      @click="togglePostPanel"
+    >
+      <!-- <div class="menu-edit-task-container" :class="{ selected: showTaskMenu }">
 				<button class="btn" @click="toggleShowTaskMenu">
 					<font-awesome-icon icon="caret-down" />
 				</button>
 				<task-menu v-if="showTaskMenu" :task="task" />
 			</div> -->
-			<div
-				class="pulse-left-indicator"
-				:style="{ backgroundColor: styleObj.clr, color: styleObj.clr }"
-			>
-				<div class="left-indicator-inner flex align-center">
-					<div class="left-indicator-checkbox" :class="{ selected: isTaskSelected }"></div>
-				</div>
-			</div>
-			<div class="task-title flex align-center">
-				<!-- <button class="btn" @click="deleteTask">X</button> -->
-				<span v-if="!showEditTask">
-					{{ task.title }}
-				</span>
-				<input
-					v-else
-					type="text"
-					ref="taskTitle"
-					v-model="taskToEdit.title"
-					@blur="saveTitle"
-					@keyup.enter="$event.target.blur()"
-				/>
-				<div class="edit-btn-wrapper">
-					<button v-if="showEditBtn" class="btn btn-edit" @click.stop="toggleEdit">EDIT</button>
-				</div>
-			</div>
-		</div>
-		<div class="grid-cells-row-component">
-			<component
-				v-for="(col, idx) in cols"
-				:key="idx"
-				:is="col.type"
-				:info="getCmpInfo(col)"
-				@update="update($event, col.type)"
-			/>
-		</div>
-		<task-update v-if="showPostPanel" :task="task" />
-	</section>
+      <div
+        class="pulse-left-indicator"
+        :style="{ backgroundColor: styleObj.clr, color: styleObj.clr }"
+      >
+        <div class="left-indicator-inner flex align-center">
+          <div
+            class="left-indicator-checkbox"
+            :class="{ selected: isTaskSelected }"
+          ></div>
+        </div>
+      </div>
+      <div class="task-title flex align-center">
+        <!-- <button class="btn" @click="deleteTask">X</button> -->
+        <span v-if="!showEditTask">
+          {{ task.title }}
+        </span>
+        <input
+          v-else
+          type="text"
+          ref="taskTitle"
+          v-model="taskToEdit.title"
+          @blur="saveTitle"
+          @keyup.enter="$event.target.blur()"
+        />
+        <div class="edit-btn-wrapper">
+          <button
+            v-if="showEditBtn"
+            class="btn btn-edit"
+            @click.stop="toggleEdit"
+          >
+            EDIT
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="grid-cells-row-component">
+      <component
+        v-for="(col, idx) in cols"
+        :key="idx"
+        :is="col.type"
+        :info="getCmpInfo(col)"
+        @update="update($event, col.type)"
+      />
+    </div>
+    <task-update
+      v-if="showPostPanel"
+      :task="task"
+      @closePanel="closePanel"
+      @saveUpdate="saveUpdate"
+    />
+  </section>
 </template>
 
 <script>
@@ -61,83 +75,92 @@ import memberPicker from '../components/member-picker';
 import taskMenu from '@/components/task-menu';
 import taskUpdate from '@/components/task-update';
 export default {
-	name: 'taskPreview',
-	props: {
-		task: {
-			type: Object,
-			required: true,
-		},
-		styleObj: {
-			type: Object,
-		},
-	},
-	data() {
-		return {
-			cols: null,
-			cmpsOrder: null,
-			taskToEdit: null,
-			showEditTask: false,
-			showEditBtn: false,
-			isTaskSelected: false,
-			isCellSelected: false,
-			showCheckBox: false,
-			showTaskMenu: false,
-			showPostPanel: false,
-		};
-	},
-	created() {
-		this.taskToEdit = { ...this.task };
-		this.cmpsOrder = this.$store.getters.cmpsOrder;
-		this.cols = this.$store.getters.cols;
-	},
-	methods: {
-		togglePostPanel() {
-			this.showPostPanel = !this.showPostPanel;
-		},
-		toggleShowTaskMenu() {
-			this.showTaskMenu = !this.showTaskMenu;
-		},
-		toggleCellSelected() {
-			this.isCellSelected = !this.isCellSelected;
-		},
-		async toggleEdit() {
-			this.toggleCellSelected();
-			await (this.showEditTask = !this.showEditTask);
-			if (this.$refs.taskTitle) this.$refs.taskTitle.focus();
-		},
-		saveTitle() {
-			this.toggleEdit();
-			this.$emit('saveTitle', this.taskToEdit);
-		},
+  name: 'taskPreview',
+  props: {
+    task: {
+      type: Object,
+      required: true,
+    },
+    styleObj: {
+      type: Object,
+    },
+  },
+  data() {
+    return {
+      cols: null,
+      cmpsOrder: null,
+      taskToEdit: null,
+      showEditTask: false,
+      showEditBtn: false,
+      isTaskSelected: false,
+      isCellSelected: false,
+      showCheckBox: false,
+      showTaskMenu: false,
+      showPostPanel: false,
+    };
+  },
+  created() {
+    this.taskToEdit = { ...this.task };
+    this.cmpsOrder = this.$store.getters.cmpsOrder;
+    this.cols = this.$store.getters.cols;
+  },
+  methods: {
+    togglePostPanel() {
+      console.log('$$$$');
+      this.showPostPanel = !this.showPostPanel;
+    },
+    toggleShowTaskMenu() {
+      this.showTaskMenu = !this.showTaskMenu;
+    },
+    toggleCellSelected() {
+      this.isCellSelected = !this.isCellSelected;
+    },
+    async toggleEdit() {
+      this.toggleCellSelected();
+      await (this.showEditTask = !this.showEditTask);
+      if (this.$refs.taskTitle) this.$refs.taskTitle.focus();
+    },
+    saveTitle() {
+      this.toggleEdit();
+      this.$emit('saveTitle', this.taskToEdit);
+    },
 
-		deleteTask() {
-			this.$emit('deleteTask', this.taskToEdit);
-		},
+    deleteTask() {
+      this.$emit('deleteTask', this.taskToEdit);
+    },
 
-		getCmpInfo(col) {
-			if (!this.task?.[col.type]) return this.getDefaultCmp(col);
+    saveUpdate(update) {
+      console.log(update);
+    },
 
-			return { selected: this.task[col.type], opts: col.data.opts };
-		},
-		getDefaultCmp(col) {
-			return {
-				selected: col.data.opts.find(({ name }) => name === 'default'),
-				opts: col.data.opts,
-			};
-		},
-		update(ev, type) {
-			const copyTask = JSON.parse(JSON.stringify(this.task));
-			copyTask[type] = ev;
-			this.$emit('updatePicker', copyTask);
-		},
-	},
-	components: {
-		statusPicker,
-		datePicker,
-		memberPicker,
-		taskMenu,
-		taskUpdate,
-	},
-	// <!-- @blur="saveTitle" -->
+    getCmpInfo(col) {
+      if (!this.task?.[col.type]) return this.getDefaultCmp(col);
+
+      return { selected: this.task[col.type], opts: col.data.opts };
+    },
+    getDefaultCmp(col) {
+      return {
+        selected: col.data.opts.find(({ name }) => name === 'default'),
+        opts: col.data.opts,
+      };
+    },
+    update(ev, type) {
+      const copyTask = JSON.parse(JSON.stringify(this.task));
+      copyTask[type] = ev;
+      this.$emit('updatePicker', copyTask);
+    },
+
+    closePanel() {
+      this.showPostPanel = false;
+    },
+  },
+  components: {
+    statusPicker,
+    datePicker,
+    memberPicker,
+    taskMenu,
+    taskUpdate,
+  },
+  // <!-- @blur="saveTitle" -->
 };
 </script>
--- src/components/task-update.vue
+++ src/components/task-update.vue
@@ -1,65 +1,76 @@
 <template>
-	<section v-if="taskToEdit" class="task-update slide-panel">
-		<header class="header-task-update flex column">
-			<h2>{{ task.title }}</h2>
-			<button>X</button>
-		</header>
-		<button class="btn new-post-placeholder" @click="toggleTextarea">Write an update</button>
-		<div v-if="showTextarea" class="flex column">
-			<textarea
-				name=""
-				id=""
-				cols="30"
-				rows="10"
-				placeholder="Write an update"
-				v-model="updateMsg"
-			></textarea>
-			<button class="btn btn-blue" @click="addUpdate">Update</button>
-		</div>
-		<div class="post-list-container">
-			<ul v-if="tasks.length" class="post-list">
-				<li v-for="task in tasks" :key="task._id" class="post">
-					<div>
-						<span>{{ task.updates }}</span>
-					</div>
-				</li>
-			</ul>
-			<div v-else class="">
-				<span>No updates yet...</span>
-			</div>
-		</div>
-	</section>
+  <section v-if="taskToEdit" class="task-update slide-panel">
+    <header class="header-task-update flex column">
+      <button class="btn-close-panel" @click="closePanel">X</button>
+      <h2>{{ task.title }}</h2>
+    </header>
+    <button
+      v-if="!showTextarea"
+      class="btn new-post-placeholder"
+      @click="toggleTextarea"
+    >
+      Write an update
+    </button>
+    <div v-if="showTextarea" class="flex column">
+      <textarea
+        name=""
+        id=""
+        cols="30"
+        rows="10"
+        placeholder="Write an update"
+        v-model="updateMsg"
+      ></textarea>
+      <button class="btn btn-blue" @click="addUpdate">Update</button>
+    </div>
+    <div class="post-list-container">
+      <ul v-if="task.updates" class="post-list">
+        <li :key="task._id" class="post">
+          <div>
+            <span>{{ task.updates }}</span>
+          </div>
+        </li>
+      </ul>
+      <div v-else class="post-no-found">
+        <span>No updates yet...</span>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-	name: 'taskUpdate',
-	props: {
-		task: {
-			type: Object,
-			required: true,
-		},
-	},
-	data() {
-		return {
-			updateMsg: '',
-			taskToEdit: null,
-			showTextarea: false,
-		};
-	},
-	created() {
-		this.taskToEdit = JSON.parse(JSON.stringify(this.$store.getters.taskToEdit));
-		console.log('he');
-	},
-	methods: {
-		addUpdate() {
-			if (!this.updateMsg) return;
-			this.$emit('saveUpdate', this.updateMsg);
-		},
-		toggleTextarea() {
-			this.showTextarea = !this.showTextarea;
-		},
-	},
-	components: {},
+  name: 'taskUpdate',
+  props: {
+    task: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      updateMsg: '',
+      taskToEdit: null,
+      showTextarea: false,
+    };
+  },
+  created() {
+    this.taskToEdit = JSON.parse(
+      JSON.stringify(this.$store.getters.taskToEdit)
+    );
+  },
+  methods: {
+    addUpdate() {
+      this.showTextarea = false;
+      if (!this.updateMsg) return;
+      this.$emit('saveUpdate', this.updateMsg);
+    },
+    toggleTextarea() {
+      this.showTextarea = !this.showTextarea;
+    },
+    closePanel() {
+      this.$emit('closePanel');
+    },
+  },
+  components: {},
 };
 </script>
