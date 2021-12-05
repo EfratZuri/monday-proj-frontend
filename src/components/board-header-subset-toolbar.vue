5c7d9a791c2e6e2eb85be226cd27a85009b95c14
Evgey <evgeny12712@gmail.com>
2021-12-05 10:58:54 +0200

    test

--- src/components/board-header-subset-toolbar.vue
+++ src/components/board-header-subset-toolbar.vue
@@ -1,28 +1,34 @@
 <template>
-	<section class="board-header-subset-toolbar">
-		<div class="monday-board-subsets-tabs">
-			<div class="monday-board-subset-item">
-				<button class="btn" @click="move">Main Table</button>
-				<div class="board-subset-item__active-strip transition-enter-done"></div>
-			</div>
-			<div class="add-board-subset-picker-wrapper">
-				<button class="btn" @click="showNewTableOptions">+ Add View</button>
-			</div>
-		</div>
-	</section>
+  <section class="board-header-subset-toolbar">
+    <div class="monday-board-subsets-tabs">
+      <div class="monday-board-subset-item active-tab">
+        <span class="main-table-icon">
+          <ion-icon name="browsers-outline"></ion-icon>
+        </span>
+        <button @click="move">Main Table</button>
+        <!-- <div
+          class="board-subset-item__active-strip transition-enter-done"
+        ></div> -->
+      </div>
+      <div @click="showNewTableOptions" class="monday-board-subset-item">
+        <span class="add-new-item-icon">+</span>
+        Add new
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-	name: 'boardHeaderSubsetToolbar',
-	methods: {
-		move() {
-			console.log('Move Function is not ready yet');
-		},
-		showNewTableOptions() {
-			console.log('MoveTableOptions Function is not ready yet');
-		},
-	},
-	components: {},
+  name: 'boardHeaderSubsetToolbar',
+  methods: {
+    move() {
+      console.log('Move Function is not ready yet');
+    },
+    showNewTableOptions() {
+      console.log('MoveTableOptions Function is not ready yet');
+    },
+  },
+  components: {},
 };
 </script>
