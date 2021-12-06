8c22241e41ade3fbd8fa2883f08326aed1816277
EfratZuri <efratzuri1@gmail.com>
2021-12-06 13:49:28 +0200

    fsd

--- src/components/task-list.vue
+++ src/components/task-list.vue
@@ -1,48 +1,35 @@
 <template>
-  <section class="task-list-container">
-    <ul v-if="tasks && tasks.length" class="task-list clean-list">
-      <draggable
-        v-model="myList"
-        :component-data="getComponentData()"
-        :list="tasks"
-        group="tasks"
-      >
-        <transition-group>
-          <li
-            v-for="task in tasks"
-            :key="task._id"
-            class="task-row flex align-center"
-          >
-            <div
-              class="menu-edit-task-container"
-              :class="{ 'dropdown-open': showTaskMenu }"
-            >
-              <button class="btn" @click="toggleShowTaskMenu">
-                <font-awesome-icon icon="caret-down" />
-              </button>
-              <task-menu
-                v-if="showTaskMenu"
-                :task="task"
-                @remove="deleteTask"
-                @duplicate="saveTask"
-                @selected="saveTask"
-              />
-            </div>
-            <task-preview
-              :task="task"
-              :styleObj="group.style"
-              @saveTitle="saveTask"
-              @deleteTask="deleteTask"
-              @updatePicker="saveTask"
-              @saveComment="saveComment"
-            />
-          </li>
-        </transition-group>
-      </draggable>
-    </ul>
-    <add-task :group="group" @addTask="saveTask" />
-    <!-- <dialogNode v-if="showDialogNode" @change="changeStatus" /> -->
-  </section>
+	<section class="task-list-container">
+		<ul v-if="tasks && tasks.length" class="task-list clean-list">
+			<draggable v-model="myList" :component-data="getComponentData()" :list="tasks" group="tasks">
+				<transition-group>
+					<li v-for="task in tasks" :key="task._id" class="task-row flex align-center">
+						<div class="menu-edit-task-container" :class="{ 'dropdown-open': showTaskMenu }">
+							<button class="btn" @click="toggleShowTaskMenu">
+								<font-awesome-icon icon="caret-down" />
+							</button>
+							<task-menu
+								v-if="showTaskMenu"
+								:task="task"
+								@remove="deleteTask"
+								@duplicate="saveTask"
+								@selected="saveTask"
+							/>
+						</div>
+						<task-preview
+							:task="task"
+							:styleObj="group.style"
+							@saveTask="saveTask"
+							@deleteTask="deleteTask"
+							@updatePicker="saveTask"
+							@saveComment="saveComment"
+						/>
+					</li>
+				</transition-group>
+			</draggable>
+		</ul>
+		<add-task :group="group" @addTask="saveTask" />
+	</section>
 </template>
 
 <script>
@@ -51,90 +38,84 @@ import taskPreview from '@/components/task-preview';
 import addTask from '@/components/add-task';
 import taskMenu from '@/components/task-menu';
 export default {
-  name: 'taskList',
-  props: {
-    group: {
-      type: Object,
-      required: true,
-    },
-  },
-  data() {
-    return {
-      taskToEdit: null,
-      tasks: [],
-      showDialogNode: false,
-      isEdit: false,
-      showTaskMenu: false,
-      activeBoard: null,
-    };
-  },
-  created() {
-    this.tasks = this.group.tasks;
-    this.activeBoard = this.$store.getters.activeBoard;
-  },
-  methods: {
-    handleChange(value) {
-      this.myList;
-      console.log(value);
-    },
+	name: 'taskList',
+	props: {
+		group: {
+			type: Object,
+			required: true,
+		},
+	},
+	data() {
+		return {
+			taskToEdit: null,
+			tasks: [],
+			showDialogNode: false,
+			isEdit: false,
+			showTaskMenu: false,
+			activeBoard: null,
+		};
+	},
+	created() {
+		this.tasks = this.group.tasks;
+		this.activeBoard = this.$store.getters.activeBoard;
+	},
+	methods: {
+		handleChange(value) {
+			this.myList;
+			console.log(value);
+		},
 
-    getComponentData() {
-      return {
-        on: {
-          change: this.handleChange,
-        },
-        attrs: {
-          wrap: true,
-        },
-        props: {
-          value: this.tasks,
-        },
-      };
-    },
-    // onDrop(dropResult) {
-    //   this.tasks = applyDrag(this.tasks, dropResult);
-    // },
-    changeStatus() {
-      console.log('Change task staus');
-    },
-    addGroup() {
-      console.log('Add new group');
-    },
-    toggleDialogNode() {
-      this.showDialogNode = !this.showDialogNode;
-    },
-    saveTask(task) {
-      this.$emit('saveTask', task, this.group._id);
-    },
-    toggleShowTaskMenu() {
-      this.showTaskMenu = false;
-      // this.showTaskMenu = !this.showTaskMenu;
-    },
+		getComponentData() {
+			return {
+				on: {
+					change: this.handleChange,
+				},
+				attrs: {
+					wrap: true,
+				},
+				props: {
+					value: this.tasks,
+				},
+			};
+		},
+		// onDrop(dropResult) {
+		//   this.tasks = applyDrag(this.tasks, dropResult);
+		// },
+		toggleDialogNode() {
+			this.showDialogNode = !this.showDialogNode;
+		},
+		saveTask(task) {
+			this.$emit('saveTask', task, this.group._id);
+		},
+		toggleShowTaskMenu() {
+			// this.showTaskMenu = false;
+			this.showTaskMenu = !this.showTaskMenu;
+		},
 
-    deleteTask(task) {
-      this.$emit('deleteTask', task, this.group._id);
-    },
-    saveComment(details) {
-      details.groupId = this.group._id;
-      this.$emit('saveComment', details);
-    },
-  },
-  computed: {
-    myList: {
-      get() {
-        console.log(this.group.tasks);
-        return this.group.tasks;
-      },
-      set() {
-        return this.$store.commit('setActiveBoard', this.activeBoard);
-      },
-    },
-  },
-  components: {
-    taskPreview,
-    addTask,
-    draggable,
-    taskMenu,
-  },
+		deleteTask(task) {
+			this.$emit('deleteTask', task, this.group._id);
+		},
+		saveComment(details) {
+			details.groupId = this.group._id;
+			this.$emit('saveComment', details);
+		},
+	},
+	computed: {
+		myList: {
+			get() {
+				console.log(this.group.tasks);
+				return this.group.tasks;
+			},
+			set() {
+				return this.$store.commit('setActiveBoard', this.activeBoard);
+			},
+		},
+	},
+	components: {
+		taskPreview,
+		addTask,
+		draggable,
+		taskMenu,
+	},
 };
 </script>
--- src/views/workspace.vue
+++ src/views/workspace.vue
@@ -1,41 +1,41 @@
 <template>
-  <div class="workspace" :class="{ 'control-open': showControlContent }">
-    <control-content
-      @toggleOpen="toggleOpenControl"
-      @add="addBoard"
-      @showBoard="showBoard"
-      @removeBoard="removeBoard"
-      :boards="boards"
-    />
+	<div class="workspace" :class="{ 'control-open': showControlContent }">
+		<control-content
+			@toggleOpen="toggleOpenControl"
+			@add="addBoard"
+			@showBoard="showBoard"
+			@removeBoard="removeBoard"
+			:boards="boards"
+		/>
 
-    <div class="workspace-content">
-      <div v-if="isLoading"><p>Loading</p></div>
+		<div class="workspace-content">
+			<div v-if="isLoading"><p>Loading</p></div>
 
-      <div v-else>
-        <boardHeader
-          :activeBoard="activeBoard"
-          @addTask="saveTask"
-          @saveBoard="saveBoard"
-          @addGroup="addGroup"
-        />
-        <groupList
-          :board="activeBoard"
-          @addGroup="addGroup"
-          @removeGroup="removeGroup"
-          @saveTask="saveTask"
-          @deleteTask="deleteTask"
-          @saveGroup="saveGroup"
-          @saveComment="saveComment"
-        />
-        <!-- <task-update
+			<div v-else>
+				<boardHeader
+					:activeBoard="activeBoard"
+					@addTask="saveTask"
+					@saveBoard="saveBoard"
+					@addGroup="addGroup"
+				/>
+				<groupList
+					:board="activeBoard"
+					@addGroup="addGroup"
+					@removeGroup="removeGroup"
+					@saveTask="saveTask"
+					@deleteTask="deleteTask"
+					@saveGroup="saveGroup"
+					@saveComment="saveComment"
+				/>
+				<!-- <task-update
 					v-if="showPostPanel"
 					:task="task"
 					@closePanel="togglePostPanel"
 					@saveUpdate="saveUpdate"
 				/> -->
-      </div>
-    </div>
-  </div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -44,65 +44,68 @@ import boardHeader from '@/components/board-header';
 import groupList from '@/components/group-list';
 
 export default {
-  name: 'workspace',
-  data() {
-    return {
-      showControlContent: false,
-    };
-  },
-  async created() {
-    this.$store.dispatch({ type: 'loadBoards' });
-  },
-  computed: {
-    isLoading() {
-      return this.$store.getters.isLoading;
-    },
-    boards() {
-      return this.$store.getters.boards;
-    },
-    activeBoard() {
-      return this.$store.getters.activeBoard;
-    },
-  },
-  methods: {
-    saveTask(details = null) {
-      this.$store.dispatch({ type: 'saveTask', details });
-    },
-    addGroup(group) {
-      this.$store.dispatch({ type: 'saveGroup', group });
-    },
-    removeGroup(group) {
-      this.$store.dispatch({ type: 'removeGroup', group });
-    },
-    saveBoard(board) {
-      this.$store.dispatch({ type: 'saveBoard', board });
-    },
-    deleteTask(task, groupId) {
-      this.$store.dispatch({ type: 'deleteTask', details: { task, groupId } });
-    },
-    saveGroup(group) {
-      this.$store.dispatch({ type: 'saveGroup', group });
-    },
-    toggleOpenControl() {
-      this.showControlContent = !this.showControlContent;
-    },
-    addBoard() {
-      this.$store.dispatch({ type: 'saveBoard' });
-    },
-    saveComment(details) {
-      this.$store.dispatch({ type: 'saveComment', details });
-    },
-    showBoard(board) {
-      this.$store.commit({ type: 'setActiveBoard', activeBoard: board });
-    },
-    removeBoard(board) {
-      this.$store.dispatch({ type: 'removeBoard', board });
-    },
-  },
-  components: {
-    groupList,
-    boardHeader,
-    controlContent,
-  },
+	name: 'workspace',
+	data() {
+		return {
+			showControlContent: false,
+		};
+	},
+	async created() {
+		this.$store.dispatch({ type: 'loadBoards' });
+	},
+	computed: {
+		isLoading() {
+			return this.$store.getters.isLoading;
+		},
+		boards() {
+			return this.$store.getters.boards;
+		},
+		activeBoard() {
+			return this.$store.getters.activeBoard;
+		},
+	},
+	methods: {
+		handleUpdates(payload) {
+			this.$store.dispatch(payload);
+		},
+		saveTask(details = null) {
+			this.$store.dispatch({ type: 'saveTask', details });
+		},
+		addGroup(group) {
+			this.$store.dispatch({ type: 'saveGroup', group });
+		},
+		removeGroup(group) {
+			this.$store.dispatch({ type: 'removeGroup', group });
+		},
+		saveBoard(board) {
+			this.$store.dispatch({ type: 'saveBoard', board });
+		},
+		deleteTask(task, groupId) {
+			this.$store.dispatch({ type: 'deleteTask', details: { task, groupId } });
+		},
+		saveGroup(group) {
+			this.$store.dispatch({ type: 'saveGroup', group });
+		},
+		toggleOpenControl() {
+			this.showControlContent = !this.showControlContent;
+		},
+		addBoard() {
+			this.$store.dispatch({ type: 'saveBoard' });
+		},
+		saveComment(details) {
+			this.$store.dispatch({ type: 'saveComment', details });
+		},
+		showBoard(board) {
+			this.$store.commit({ type: 'setActiveBoard', activeBoard: board });
+		},
+		removeBoard(board) {
+			this.$store.dispatch({ type: 'removeBoard', board });
+		},
+	},
+	components: {
+		groupList,
+		boardHeader,
+		controlContent,
+	},
 };
 </script>
