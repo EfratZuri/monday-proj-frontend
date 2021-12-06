f34518c068ca38caeba400bbd466f93bd8de2dc0
EfratZuri <efratzuri1@gmail.com>
2021-12-06 12:04:24 +0200

    Chnages in task menu

--- src/components/tag-picker.vue
+++ src/components/tag-picker.vue
@@ -5,6 +5,14 @@
 	>
 		<div class="col-cell" @click="toggleOptions">
 			<div v-if="selectedCopy && selectedCopy.length">
+				<!-- <span :style="selectedCopy[0].style">{{ tagForDisplay(selectedCopy[0].txt) }}</span>
+				<span v-if="selectedCopy[1]" :style="selectedCopy[1].style">{{
+					tagForDisplay(selectedCopy[1].txt)
+				}}</span>
+				<div v-if=""></div>
+				<span v-if="selectedCopy[2]" :style="selectedCopy[0].style">{{
+					tagForDisplay(selectedCopy[0].txt)
+				}}</span> -->
 				<span v-for="(tag, idx) in selectedCopy" :key="idx" :style="tag.style">
 					{{ tagForDisplay(tag.txt) }}
 				</span>
@@ -37,6 +45,7 @@
 </template>
 
 <script>
+import { utilService } from '../services/util.service.js';
 export default {
 	name: 'tagPicker',
 	props: ['info'],
@@ -49,6 +58,9 @@ export default {
 	},
 	created() {
 		this.selectedCopy = JSON.parse(JSON.stringify(this.info.selected));
+		// this.selectedCopyTrimed = JSON.parse(JSON.stringify(this.info.selected));
+		const clrs = this.$store.getters.clrs;
+		this.tagToEdit.style.color = clrs[utilService.getRandomInt(0, clrs.length - 1)];
 	},
 	methods: {
 		update() {
@@ -59,8 +71,8 @@ export default {
 		toggleOptions() {
 			this.showOptions = !this.showOptions;
 		},
-		tagForDisplay(tag) {
-			return `#${tag.txt}`;
+		tagForDisplay(txt) {
+			return `#${txt}`;
 		},
 	},
 };
--- src/components/task-menu.vue
+++ src/components/task-menu.vue
@@ -1,11 +1,37 @@
 <template>
 	<section class="task-menu-container">
-		<div class="menu-item">Open</div>
+		<button class="btn btn-icon menu-item">
+			<font-awesome-icon icon="arrows-alt-v" />
+			<span>Open</span>
+		</button>
+		<button class="btn btn-icon menu-item" @click="select">Select item</button>
 		<div class="menu-item" @click="remove"><span>Delete</span></div>
+		<button class="btn btn-icon menu-item" @click="duplicate">
+			<font-awesome-icon icon="copy" />
+			<span>Duplicate</span>
+		</button>
+		<div class="btn btn-relative">
+			<button class="btn btn-icon menu-item" @mousemove="toggleMoveToOptions">
+				<font-awesome-icon icon="arrow-right" />
+				<span>Move To</span>
+			</button>
+			<!-- <move-to-opts v-if="showMoveToOptions" />
+			<div>
+				<button class="btn opt-item" @click="">Move to top</button>
+				<button class="btn opt-item">Move to group</button>
+				<button class="btn opt-item">Move to board</button>
+			</div> -->
+		</div>
+
+		<button class="btn btn-icon menu-item" @click="remove">
+			<font-awesome-icon icon="trash-alt" />
+			<span>Delete</span>
+		</button>
 	</section>
 </template>
 
 <script>
+// import moveToOpts from '../components/move-to-opts.vue';
 export default {
 	name: 'taskMenu',
 	props: {
@@ -14,13 +40,27 @@ export default {
 			required: true,
 		},
 	},
-	created() {},
-	computed: {},
+	data() {
+		return {
+			showMoveToOptions: false,
+		};
+	},
 	methods: {
+		toggleMoveToOptions() {
+			this.showMoveToOptions = !this.showMoveToOptions;
+		},
 		remove() {
-			console.log('removw task');
+			this.$emit('remove', this.task);
+		},
+		select() {
+			this.$emit('select', this.task);
+		},
+		duplicate() {
+			this.$emit('duplicate', this.task);
 		},
 	},
-	components: {},
+	components: {
+		// moveToOpts,
+	},
 };
 </script>
