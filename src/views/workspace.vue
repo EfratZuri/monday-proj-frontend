44aae77ca83a63b935e664655b18739296d8ee87
EfratZuri <efratzuri1@gmail.com>
2021-12-07 11:15:41 +0200

    emits from board header

--- src/views/workspace.vue
+++ src/views/workspace.vue
@@ -1,38 +1,39 @@
 <template>
-  <div class="workspace" :class="{ 'control-open': showControlContent }">
-    <control-content
-      @toggleOpen="toggleOpenControl"
-      @add="addBoard"
-      @showBoard="showBoard"
-      @removeBoard="removeBoard"
-      :boards="boards"
-    />
+	<div class="workspace" :class="{ 'control-open': showControlContent }">
+		<control-content
+			@toggleOpen="toggleOpenControl"
+			@add="addBoard"
+			@showBoard="showBoard"
+			@removeBoard="removeBoard"
+			:boards="boards"
+		/>
 
-    <div class="workspace-content">
-      <div v-if="isLoading"><p>Loading</p></div>
+		<div class="workspace-content">
+			<div v-if="isLoading"><p>Loading</p></div>
 
-      <div v-else>
-        <boardHeader
-          :activeBoard="activeBoard"
-          @addTask="saveTask"
-          @saveBoard="saveBoard"
-          @addGroup="addGroup"
-        />
-        <groupList
-          :board="activeBoard"
-          :boards="boards"
-          @addGroup="addGroup"
-          @removeGroup="removeGroup"
-          @moveToBoard="moveGroupToBoard"
-          @saveTask="saveTask"
-          @deleteTask="deleteTask"
-          @saveGroup="saveGroup"
-          @saveComment="saveComment"
-          @duplicateGroup="duplicateGroup"
-          @setSelected="setSelected"
-        />
-        <selected-task v-if="tasks.length" :tasks="tasks" />
-        <!-- <task-update
+			<div v-else>
+				<boardHeader
+					:activeBoard="activeBoard"
+					@addTask="saveTask"
+					@saveBoard="saveBoard"
+					@addGroup="addGroup"
+				/>
+				<groupList
+					:board="activeBoard"
+					:boards="boards"
+					@addGroup="addGroup"
+					@removeGroup="removeGroup"
+					@moveToBoard="moveGroupToBoard"
+					@saveTask="saveTask"
+					@deleteTask="deleteTask"
+					@saveGroup="saveGroup"
+					@saveComment="saveComment"
+					@duplicateGroup="duplicateGroup"
+					@setSelected="setSelected"
+					@addColumn="addColumn"
+				/>
+				<selected-task v-if="tasks.length" :tasks="tasks" />
+				<!-- <task-update
 			<div v-else>
 				<boardHeader
 					:activeBoard="activeBoard"
@@ -56,9 +57,9 @@
 					@closePanel="togglePostPanel"
 					@saveUpdate="saveUpdate"
 				/> -->
-      </div>
-    </div>
-  </div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -68,81 +69,84 @@ import groupList from '@/components/group-list';
 import selectedTask from '@/components/selected-task.vue';
 
 export default {
-  name: 'workspace',
-  data() {
-    return {
-      showControlContent: false,
-      tasks: [],
-    };
-  },
-  async created() {
-    this.$store.dispatch({ type: 'loadBoards' });
-  },
-  computed: {
-    isLoading() {
-      return this.$store.getters.isLoading;
-    },
-    boards() {
-      return this.$store.getters.boards;
-    },
-    activeBoard() {
-      return this.$store.getters.activeBoard;
-    },
-  },
-  methods: {
-    saveTask(details = null) {
-      this.$store.dispatch({ type: 'saveTask', details });
-    },
-    addGroup(group) {
-      this.$store.dispatch({ type: 'saveGroup', group });
-    },
-    removeGroup(group) {
-      this.$store.dispatch({ type: 'removeGroup', group });
-    },
-    saveBoard(board) {
-      this.$store.dispatch({ type: 'saveBoard', board });
-    },
-    deleteTask(task, groupId) {
-      this.$store.dispatch({ type: 'deleteTask', details: { task, groupId } });
-    },
-    saveGroup(group) {
-      this.$store.dispatch({ type: 'saveGroup', group });
-    },
-    duplicateGroup(group) {
-      this.$store.dispatch({ type: 'duplicateGroup', group });
-    },
-    moveGroupToBoard(moveDetails) {
-      this.$store.dispatch({ type: 'moveGroupToBoard', moveDetails });
-    },
-    toggleOpenControl() {
-      this.showControlContent = !this.showControlContent;
-    },
-    addBoard() {
-      this.$store.dispatch({ type: 'saveBoard' });
-    },
-    saveComment(details) {
-      this.$store.dispatch({ type: 'saveComment', details });
-    },
-    showBoard(board) {
-      this.$store.commit({ type: 'setActiveBoard', activeBoard: board });
-    },
-    removeBoard(board) {
-      this.$store.dispatch({ type: 'removeBoard', board });
-    },
-    setSelected(task, boolean) {
-      console.log(task, boolean);
-      const idx = this.tasks.findIndex(({ id }) => id === task.id);
-      if (boolean) this.tasks.push(task);
-      else this.tasks.splice(idx, 1);
-      console.log(this.tasks);
-    },
-  },
-  components: {
-    groupList,
-    boardHeader,
-    controlContent,
-    selectedTask,
-  },
+	name: 'workspace',
+	data() {
+		return {
+			showControlContent: false,
+			tasks: [],
+		};
+	},
+	async created() {
+		this.$store.dispatch({ type: 'loadBoards' });
+	},
+	computed: {
+		isLoading() {
+			return this.$store.getters.isLoading;
+		},
+		boards() {
+			return this.$store.getters.boards;
+		},
+		activeBoard() {
+			return this.$store.getters.activeBoard;
+		},
+	},
+	methods: {
+		addColumn(columnType) {
+			this.$store.dispatch({ type: 'addColumn', columnType });
+		},
+		saveTask(details = null) {
+			this.$store.dispatch({ type: 'saveTask', details });
+		},
+		addGroup(group) {
+			this.$store.dispatch({ type: 'saveGroup', group });
+		},
+		removeGroup(group) {
+			this.$store.dispatch({ type: 'removeGroup', group });
+		},
+		saveBoard(board) {
+			this.$store.dispatch({ type: 'saveBoard', board });
+		},
+		deleteTask(task, groupId) {
+			this.$store.dispatch({ type: 'deleteTask', details: { task, groupId } });
+		},
+		saveGroup(group) {
+			this.$store.dispatch({ type: 'saveGroup', group });
+		},
+		duplicateGroup(group) {
+			this.$store.dispatch({ type: 'duplicateGroup', group });
+		},
+		moveGroupToBoard(moveDetails) {
+			this.$store.dispatch({ type: 'moveGroupToBoard', moveDetails });
+		},
+		toggleOpenControl() {
+			this.showControlContent = !this.showControlContent;
+		},
+		addBoard() {
+			this.$store.dispatch({ type: 'saveBoard' });
+		},
+		saveComment(details) {
+			this.$store.dispatch({ type: 'saveComment', details });
+		},
+		showBoard(board) {
+			this.$store.commit({ type: 'setActiveBoard', activeBoard: board });
+		},
+		removeBoard(board) {
+			this.$store.dispatch({ type: 'removeBoard', board });
+		},
+		setSelected(task, boolean) {
+			console.log(task, boolean);
+			const idx = this.tasks.findIndex(({ id }) => id === task.id);
+			if (boolean) this.tasks.push(task);
+			else this.tasks.splice(idx, 1);
+			console.log(this.tasks);
+		},
+	},
+	components: {
+		groupList,
+		boardHeader,
+		controlContent,
+		selectedTask,
+	},
 };
 </script>
 
