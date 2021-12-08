4f9a73963dd6642c1e80870797df15534c251397
EfratZuri <efratzuri1@gmail.com>
2021-12-08 15:26:12 +0200

    visual changeds in control contant compenent

--- src/components/board-header-filter-search.vue
+++ src/components/board-header-filter-search.vue
@@ -1,29 +1,25 @@
 <template>
-  <div
-    class="search-tasks-wrapper"
-    @click="toggleInput"
-    :class="{ 'open-input': isOpenInput }"
-  >
-    <div class="search-icon-wrapper">
-      <ion-icon name="search-outline"></ion-icon>
-    </div>
-    <input class="search-input" type="search" placeholder="Search" />
-  </div>
+	<div class="search-tasks-wrapper" @click="toggleInput" :class="{ 'open-input': isOpenInput }">
+		<div class="search-icon-wrapper">
+			<ion-icon name="search-outline"></ion-icon>
+		</div>
+		<input class="search-input" type="search" placeholder="Search" />
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'board-header-filter-search',
-  data() {
-    return {
-      isOpenInput: false,
-    };
-  },
-  methods: {
-    toggleInput() {
-      this.isOpenInput = !this.isOpenInput;
-    },
-  },
+	name: 'board-header-filter-search',
+	data() {
+		return {
+			isOpenInput: false,
+		};
+	},
+	methods: {
+		toggleInput() {
+			this.isOpenInput = !this.isOpenInput;
+		},
+	},
 };
 </script>
 
