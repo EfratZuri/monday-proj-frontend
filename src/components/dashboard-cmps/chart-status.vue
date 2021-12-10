b88eab1ee60934a2e94823e01a110d3190097392
EfratZuri <efratzuri1@gmail.com>
2021-12-10 15:44:54 +0200

    cx

--- src/components/dashboard-cmps/chart-status.vue
+++ src/components/dashboard-cmps/chart-status.vue
@@ -1,30 +1,30 @@
 <script>
 import { Bar } from 'vue-chartjs';
 export default {
-  name: 'chartStatus',
-  extends: Bar,
-  data() {
-    return {
-      boards: null,
-      options: {
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-              },
-            },
-          ],
-          xAxes: [
-            {
-              ticks: {
-                fontSize: 20,
-              },
-            },
-          ],
-        },
-      },
-    };
-  },
+	name: 'chartStatus',
+	extends: Bar,
+	data() {
+		return {
+			boards: null,
+			options: {
+				scales: {
+					yAxes: [
+						{
+							ticks: {
+								beginAtZero: true,
+							},
+						},
+					],
+					xAxes: [
+						{
+							ticks: {
+								fontSize: 20,
+							},
+						},
+					],
+				},
+			},
+		};
+	},
 };
 </script>
